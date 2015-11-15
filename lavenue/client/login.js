34774632d33aa78a188890625d803fcd32396d02
kangsoontan <tankangsoon@gmail.com>
2015-11-15 14:20:03 -0800

    unsettled login with validation (see console log)

---
+++ lavenue/client/login.js
@@ -0,0 +1,35 @@
+Template.Login.events({  
+  'submit .login-form': function (event, template) {
+    event.preventDefault();
+
+    var $form = $(event.currentTarget);
+    var $emailInput = $form.find('.email-address-input').eq(0);
+    var $passwordInput = $form.find('.password-input').eq(0);
+
+    var emailAddress = $emailInput.val() || '';
+    var password = $passwordInput.val() || '';
+
+    //trim
+    emailAddress = emailAddress.replace(/^\s*|\s*$/g, '');
+    password = password.replace(/^\s*|\s*$/g, '');
+
+    //validate
+    var isValidEmail = checkEmailIsValid(emailAddress);
+    var isValidPassword = checkPasswordIsValid(password);
+
+    if (!isValidEmail || !isValidPassword) {
+      if (!isValidEmail) {
+        console.log("Invalid email");
+      }
+      if (!isValidPassword) {
+        console.log("Invalid password, must be at least 8 characters long");
+      }
+    } else {
+      Meteor.loginWithPassword(emailAddress, password, function (error) {
+        if (error) {
+          console.log('Account login failed for unknown reasons :(');
+        }
+      });
+    }
+  }
+});
---
+++ lavenue/client/signup.js
@@ -0,0 +1,38 @@
+Template.SignUp.events({  
+  'submit .sign-up-form': function (event, template) {
+    event.preventDefault();
+
+    var $form = $(event.currentTarget);
+    var $emailInput = $form.find('.email-address-input').eq(0);
+    var $passwordInput = $form.find('.password-input').eq(0);
+
+    var emailAddress = $emailInput.val() || '';
+    var password = $passwordInput.val() || '';
+
+    //trim
+    emailAddress = emailAddress.replace(/^\s*|\s*$/g, '');
+    password = password.replace(/^\s*|\s*$/g, '');
+
+    //validate
+    var isValidEmail = checkEmailIsValid(emailAddress);
+    var isValidPassword = checkPasswordIsValid(password);
+
+    if (!isValidEmail || !isValidPassword) {
+      if (!isValidEmail) {
+        console.log("Invalid email");
+      }
+      if (!isValidPassword) {
+        console.log("Invalid password, must be at least 8 characters long");
+      }
+    } else {
+      Accounts.createUser({
+        email: emailAddress,
+        password: password
+      }, function (error) {
+        if (error) {
+          console.log('Account creation failed for unknown reasons :(');
+        }
+      });
+    }
+  }
+});
