caacb0445d016973eb8ae6bb1dac767019e254c4
kangsoontan <tankangsoon@gmail.com>
2015-11-21 21:50:21 -0800

    Basic template for browse page

--- lavenue/client/home.js
+++ lavenue/client/home.js
@@ -1,3 +1,11 @@
+Template.home.onRendered(function(){
+	$('.special.card .image').dimmer({
+	  on: 'hover'
+	});
+	$('.card').transition('horizontal flip');
+	$('.card').transition('horizontal flip');
+});
+
 Template.home.helpers({
 	browseImages: function() {
 		return Images.find().fetch();
--- lavenue/lib/collections.js
+++ lavenue/lib/collections.js
@@ -4,5 +4,5 @@ Images = new Mongo.Collection('images');
 //debug code
 Images.remove({});
 for (i = 0; i < 30; i++) {
-	Images.insert({imageUrl: "images/model.png", description: "Am I pretty?"});
+	Images.insert({imageUrl: "images/model.png", description: "Am I pretty?", price: "$9.99", likes: "99", style: "Classy urban"});
 }
