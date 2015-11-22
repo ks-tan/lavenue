5e370f2bc478d52d431969d44af6d21104f482f8
kangsoontan <tankangsoon@gmail.com>
2015-11-22 12:06:30 -0800

    debug

--- lavenue/lib/collections.js
+++ lavenue/lib/collections.js
@@ -1,8 +1,1 @@
 Images = new Mongo.Collection('images');
-
-
-//debug code
-Images.remove({});
-for (i = 0; i < 30; i++) {
-	Images.insert({seller: "ChickyGirl85", imageUrl: "images/model.png", description: "Blue jacket and cool looking scarf makes you look classy while keeping you warm", price: "$9.99", likes: "99", style: "Classy urban"});
-}
