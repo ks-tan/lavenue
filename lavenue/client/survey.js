bd17de20a70a2ba4ede409996c01397994d56866
kangsoontan <tankangsoon@gmail.com>
2015-11-22 22:05:55 -0800

    Dynamic looking graph with hard-coded data

--- lavenue/client/survey.js
+++ lavenue/client/survey.js
@@ -1,5 +1,7 @@
 var numOfPicturesSwiped = 0;
 
+Session.set("surveyDone", false);
+
 Template.survey.onRendered(function(){
 	numOfPicturesSwiped = 0;
 	$('.special.card .image').dimmer({
@@ -7,18 +9,24 @@ Template.survey.onRendered(function(){
 	});
 	$('.surveyCard').hide();
 	$('.surveyCard').first().transition('horizontal flip').addClass('shown');
-	$('#surveyDoneMsg').hide();
 });
 
 Template.survey.helpers({
 	surveyImages: function(){
 		return Images.find().fetch();
+	},
+	whichOne: function(){
+		if (Session.get("surveyDone") == true){
+			return 'surveyDoneMsg';
+		}
 	}
 });
 
 Template.survey.events({
 	'click .ui.big.button': function(){
-		if (numOfPicturesSwiped == 4){
+		if (numOfPicturesSwiped == 5){
+			Session.set("surveyDone", true);
+			numOfPicturesSwiped = 0;
 			$('.surveyCard.shown').last().transition('horizontal flip');
 			$('#analysing').transition('fade up');
 			setTimeout(function(){
@@ -26,6 +34,7 @@ Template.survey.events({
 				$('#surveyDoneMsg').transition('fade up');
 			}, 600);
 		} else {
+			Session.set("surveyDone", false);
 			$('.surveyCard.shown').last().transition('horizontal flip');
 			setTimeout(function(){
 				$('.surveyCard.shown').last().next().transition('horizontal flip').addClass("shown");
@@ -37,8 +46,54 @@ Template.survey.events({
 		$('#surveyDoneMsg').transition('fade up');
 		$('#surveyDoneMsg').hide();
 		setTimeout(function(){
+			$('#analysing').transition('fade up');
 			$('.surveyCard.shown').last().next().transition('horizontal flip').addClass("shown");
 		}, 600);
 		numOfPicturesSwiped++;
 	}
 });
+
+Template.surveyDoneMsg.onRendered(function(){
+	$('#surveyDoneMsg').hide();
+});
+
+Template.surveyDoneMsg.topGenresChart = function() {
+    return {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: "You are <b>Classic!</b>"
+        },
+        tooltip: {
+            pointFormat: '<b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    },
+                    connectorColor: 'silver'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'genre',
+            data: [
+                ['Classic',   50],
+                ['Chic',       20],
+                ['Bohemian',   15],
+                ['Avant-garde',    10],
+                ['Whimsical',     5]
+            ]
+        }]
+    };
+};
