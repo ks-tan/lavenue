820f5e6cc1e380c41a438625ca0dbc69cc2b0a33
kangsoontan <tankangsoon@gmail.com>
2015-11-22 21:07:22 -0800

    Styling the survey page

--- lavenue/client/survey.js
+++ lavenue/client/survey.js
@@ -1,16 +1,13 @@
 var numOfPicturesSwiped = 0;
 
 Template.survey.onRendered(function(){
+	numOfPicturesSwiped = 0;
 	$('.special.card .image').dimmer({
 	  on: 'hover'
 	});
 	$('.surveyCard').hide();
 	$('.surveyCard').first().transition('horizontal flip').addClass('shown');
-	if (numOfPicturesSwiped < 5){
-		$('#surveyDoneMsg').hide();
-	} else {
-		$('#analysing').hide();
-	}
+	$('#surveyDoneMsg').hide();
 });
 
 Template.survey.helpers({
@@ -21,20 +18,27 @@ Template.survey.helpers({
 
 Template.survey.events({
 	'click .ui.big.button': function(){
-		$('.surveyCard.shown').last().transition('horizontal flip');
-		setTimeout(function(){
-			$('.surveyCard.shown').last().next().transition('horizontal flip').addClass("shown");
-		}, 600);
-		numOfPicturesSwiped++;
-		if (numOfPicturesSwiped == 5){
+		if (numOfPicturesSwiped == 4){
+			$('.surveyCard.shown').last().transition('horizontal flip');
 			$('#analysing').transition('fade up');
 			setTimeout(function(){
 				$('#analysing').hide();
 				$('#surveyDoneMsg').transition('fade up');
 			}, 600);
+		} else {
+			$('.surveyCard.shown').last().transition('horizontal flip');
+			setTimeout(function(){
+				$('.surveyCard.shown').last().next().transition('horizontal flip').addClass("shown");
+			}, 600);
+			numOfPicturesSwiped++;
 		}
 	},
 	'click #goLater': function() {
-		$('#surveyDoneMsg').transition('fade up');		
+		$('#surveyDoneMsg').transition('fade up');
+		$('#surveyDoneMsg').hide();
+		setTimeout(function(){
+			$('.surveyCard.shown').last().next().transition('horizontal flip').addClass("shown");
+		}, 600);
+		numOfPicturesSwiped++;
 	}
 });
