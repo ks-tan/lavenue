c02d1be7e0ea7df93b6c36fb19c872f3c029c8e1
kangsoontan <tankangsoon@gmail.com>
2015-12-05 17:30:01 -0800

    Lots of changes to wardrobe

--- lavenue/client/wardrobe.js
+++ lavenue/client/wardrobe.js
@@ -3,6 +3,9 @@ Template.wardrobe.onRendered(function(){
 		animation: 'fade up',
 		duration: '1s'
 	});
+	$('.special.card .image').dimmer({
+	  on: 'hover'
+	});
 });
 
 Template.wardrobe.helpers({
@@ -23,43 +26,129 @@ Template.wardrobe.helpers({
 		return myPurchases;
 	},
 	imageUrl: function() {
-		return Session.get("shopImages");
+		return ShopItems.find({}).fetch();
 	},
 	getImageFromInstagram: function() {
-		Meteor.call("getImageInfo", "https://api.instagram.com/v1/users/382872795/media/recent/?client_id=36a3a03c63914bb48edb68724a33e548", function(err, result){
-			if (typeof err !== "undefined") {
-				console.log(err);
-			}
+		if (ShopItems.find().count() == 0 || ShopItems == null){
+			Meteor.call("getImageInfo", "https://api.instagram.com/v1/users/382872795/media/recent/?client_id=36a3a03c63914bb48edb68724a33e548", function(err, result){
+				if (typeof err !== "undefined") {
+					console.log(err);
+				}
 
-			if (typeof result === "undefined") {
-				return;
-			}
-				
-			imagesData = result.data.data
-			if (typeof imagesData === "undefined") {
-				return;
-			}
+				if (typeof result === "undefined") {
+					return;
+				}
+					
+				imagesData = result.data.data
+				if (typeof imagesData === "undefined") {
+					return;
+				}
 
-			// get images with certain tag
-			var shopImages = [];
-			for (x in imagesData) {
-				// var tags = imagesData[x].tags;
+				// get images with certain tag
+				// var shopImages = [];
+				for (x in imagesData) {
+					// var tags = imagesData[x].tags;
 
-				// if (tags.length <= 0) {
-				// 	continue;
-				// }
-				
-				shopImages.push(imagesData[x].images.standard_resolution.url);
-				// for (y in tags) {
-				// 	// change the tag to correct one
-				// 	if (tags[y] === "inktober") {
-				// 		var url = imagesData[x].images.standard_resolution.url;
-				// 		console.log(url);
-				// 		Session.set("shopImage", url);
-				// 	}
-				// }
-			}
-			Session.set("shopImages", shopImages);
-		});
+					// if (tags.length <= 0) {
+					// 	continue;
+					// }
+					
+					// shopImages.push(imagesData[x].images.standard_resolution.url);
+					imageUrl = imagesData[x].images.standard_resolution.url;
+					rand = Math.floor((Math.random() * 3) + 1);
+					if (rand == 3){
+						rent = "yes";
+						publish = "yes";
+					}else{
+						rent = "no";
+						publish = "no";
+					}
+					console.log(imageUrl);
+					ShopItems.insert({
+						imageUrl: imageUrl, 
+						description: "", 
+						price: "", 
+						likes: "", 
+						style: "",
+						publish: publish,
+						rented: rent
+					});
+					// for (y in tags) {
+					// 	// change the tag to correct one
+					// 	if (tags[y] === "inktober") {
+					// 		var url = imagesData[x].images.standard_resolution.url;
+					// 		console.log(url);
+					// 		Session.set("shopImage", url);
+					// 	}
+					// }
+				}
+			});
+		}	
 	}
 });
+
+Template.yourShopCard.helpers({
+	hasPrice: function(){
+		if (this.price==""){
+			return false;
+		}
+		return true;
+	},
+	isPublished: function(){
+		if (this.publish != "yes"){
+			return false;
+		}
+		return true;
+	},
+	isStyleIndicated: function(){
+		if (this.style==""){
+			return false;
+		}
+		return true;
+	},
+	isDescriptionEntered: function(){
+		if (this.description == ""){
+			return false;
+		}
+		return true;
+	},
+	isRented: function(){
+		if (this.rented != "yes"){
+			return false;
+		}
+		return true;
+	}
+});
+
+Template.yourShopCard.events({
+	"submit #insertPrice": function(event){
+		event.preventDefault();
+		var price = event.target.text.value;
+		var id=this._id;
+		ShopItems.update(id, {$set: {price: price}});
+	},
+	"click #publishButton": function(event){
+		var id=this._id;
+		ShopItems.update(id, {$set: {publish: "yes"}});
+	},
+	"submit #insertDescription": function(event){
+		event.preventDefault();
+	    var x = document.getElementById("insertDescription");
+	    var description = "";
+	    var i;
+	    for (i = 0; i < x.length ;i++) {
+	    	if (i == 0){
+	    		var bust = x.elements[i].value;
+	    	} else if (i == 1){
+	    		var waist = x.elements[i].value;
+	    	} else if (i == 2) {
+	    		var hip = x.elements[i].value;
+	    	}
+	    }
+	    var id = this._id
+	    var description = document.getElementById(id+'insertDescription').value;
+	    ShopItems.update(id, {$set: {bust: bust, waist: waist, hip: hip, description: description}});
+	    console.log(description);
+	}
+});
+
--- lavenue/lib/collections.js
+++ lavenue/lib/collections.js
@@ -2,3 +2,4 @@ Items = new Mongo.Collection('items');
 Cart = new Mongo.Collection('cart');
 Profile = new Mongo.Collection('profile');
 Purchases = new Mongo.Collection('purchases');
+ShopItems = new Mongo.Collection('shopItems');
